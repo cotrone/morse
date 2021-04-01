792b2e75037b70fd17d8b16df04bf12459fd6134
davean <davean@xkcd.com>
2021-03-31 23:33:49 -0400

    Send data as morse code.

--- src/Morse/Web.hs
+++ src/Morse/Web.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeOperators #-}
@@ -10,9 +11,15 @@ module Morse.Web
  ) where
 
 import           Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.Encoding as TLE
 import           Data.UUID (UUID)
+import qualified Data.UUID as UUID
 import           Morse.API
+import           Morse.Encoding
 import           Morse.Types
+import Network.HTTP.Media ((//), (/:))
 import qualified Network.Wai as WAI
 import           Servant
 
@@ -20,8 +27,8 @@ type MorseAPI
  = MorseSay
 
 type MorseSay
-  =    "say" :> Capture "uuid" UUID :> Capture "phrase" Text :> Get '[JSON] MorseResponse
-  :<|> "say" :>                        Capture "phrase" Text :> Get '[JSON] MorseResponse
+  =    "say" :> Capture "uuid" UUID :> Capture "phrase" Text :> Get '[MorseText] MorseResponse
+  :<|> "say" :>                        Capture "phrase" Text :> Get '[MorseText] MorseResponse
 
 morseAPI :: Proxy MorseAPI
 morseAPI = Proxy
@@ -43,3 +50,12 @@ sayWithout = say Nothing
 say :: Morse m => Maybe UUID -> Text -> m MorseResponse
 say fromState phrase = do
   lookupMorse ("Give me a user token here!"::Text) (fromState, phrase)
+
+data MorseText
+
+instance Accept MorseText where
+  contentType _ = "text" //"x-morse" /: ("charset", "utf-8")
+
+instance MimeRender MorseText MorseResponse where
+  mimeRender _ (MorseResponse msg ss) = TLE.encodeUtf8 . TL.fromStrict . encodeMorse $ T.concat [UUID.toText ss, " ", msg]
+
